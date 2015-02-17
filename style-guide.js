057e25c6158cfd8310f191609087c3d06642d2c4
Lincoln Anderson <landerson@352inc.com>
2015-02-17 17:37:13 -0500

    Adding quick nav code and data-repeat example. Split CSS out a little bit.

--- style-guide.js
+++ style-guide.js
@@ -1,11 +1,35 @@
 $.SyntaxHighlighter.init();
 
+//duplicate repeating blocks
+$('[data-repeat]').each(function(index){
+	var num = Number($(this).data('repeat')) - 1;
+	$(this).removeAttr('data-repeat');
+	for(var i=0;i<num;i++) {
+		$(this).after($(this).clone());
+	}
+});
+
 //find all blocks to source
 $('[data-showsource]').each(function(index){
-	var src = $(this).html(); //scrape HTML
-	var block = $('<pre class="language-html"/>').text(src); //inject into a code block
-	$(this).after(block); //inject on page
+	var src = $(this).html();
+	var block = $('<pre class="language-html"/>').text(src);
+	$(this).after(block);
 });
+
+// Generate a quick navigation menu
+(function() {
+	var $quickNav = $('.js-quick-nav');
+	
+	$('.js-quick-nav-anchor').each(function(index){
+		var $this = $(this),
+			anchorName = $this.prop('id') || 'quick-nav-anchor-' + index;
+		$this.prop('id', anchorName);
+		
+		$quickNav.append('<li><a href="#' + anchorName + '">' + $this.text() + '</a></li>');
+		
+        $this.append(' <a href="#top">Top</a>');
+	});
+}());
 
 //wait a bit to avoid FOUC
 //would be better if SyntaxHighlighter had an onComplete callback
